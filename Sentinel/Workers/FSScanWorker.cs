7d91d2a452db228585ce6c11d712cca7e2d2ef21
Tabing010102 <tabing010102@outlook.com>
2024-12-27 22:40:45 +0800

    chore: add FSScanWatcher part 2 (shelved)

--- Sentinel/Workers/FSScanWorker.cs
+++ Sentinel/Workers/FSScanWorker.cs
@@ -1,137 +1,25 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Sentinel.Plugin.Configs;
+﻿using Microsoft.Extensions.Logging;
 using Sentinel.Plugin.Contracts;
-using Sentinel.Plugin.Models;
 
 namespace Sentinel.Workers
 {
-    public class FSScanWorker : BackgroundService
+    public class FSScanWorker
     {
-        private readonly SemaphoreSlim _semaphore = new(1, 1);
-
-        private readonly ILogger<FSScanWorker> _logger;
+        private readonly ILogger<FSWatchWorker> _logger;
         private readonly SentinelDbContext _dbContext;
-
-        private bool _configured;
         private IPlugin _plugin = null!;
-        private PluginConfigBase _pluginConfig = null!;
 
-        private IQueryable<AppBinary> _appBinaries = null!;
-
-        public FSScanWorker(ILogger<FSScanWorker> logger, SentinelDbContext dbContext)
+        public FSScanWorker(ILogger<FSWatchWorker> logger, SentinelDbContext dbContext, IPlugin plugin)
         {
             _logger = logger;
             _dbContext = dbContext;
-            _configured = false;
-        }
 
-        public void Configure(IPlugin plugin, PluginConfigBase config)
-        {
             _plugin = plugin ?? throw new ArgumentNullException(nameof(plugin));
-            _pluginConfig = config ?? throw new ArgumentNullException(nameof(config));
-            _plugin.Config = _pluginConfig;
-            _configured = true;
         }
 
-        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        public async void ExecuteAsync(CancellationToken stoppingToken)
         {
-            if (!_configured)
-            {
-                throw new InvalidOperationException("FSScanWorker is not configured.");
-            }
-
-            _appBinaries = _dbContext.AppBinaries
-                .Include(x => x.Files)
-                .ThenInclude(x => x.Chunks)
-                .Select(x => x.ToPluginModel());
-
-            var tcs = new TaskCompletionSource();
-
-            using (var watcher = new FileSystemWatcher(_pluginConfig.LibraryFolder))
-            {
-                watcher.NotifyFilter = NotifyFilters.DirectoryName
-                                     | NotifyFilters.FileName
-                                     | NotifyFilters.LastWrite
-                                     | NotifyFilters.Size;
-
-                watcher.Changed += async (sender, e) => await OnChangedAsync(sender, e, stoppingToken);
-                watcher.Created += async (sender, e) => await OnCreatedAsync(sender, e, stoppingToken);
-                watcher.Deleted += async (sender, e) => await OnDeletedAsync(sender, e, stoppingToken);
-                watcher.Renamed += async (sender, e) => await OnRenamedAsync(sender, e, stoppingToken);
-                watcher.Error += (sender, e) => _logger.LogError(e.GetException(), "Error in FileSystemWatcher");
-
-                watcher.Filter = "*";
-                watcher.IncludeSubdirectories = true;
-                watcher.EnableRaisingEvents = true;
-
-                stoppingToken.Register(() =>
-                {
-                    watcher.EnableRaisingEvents = false;
-                    tcs.SetResult();
-                });
-            }
-
-            return tcs.Task;
-        }
-
-        private async Task OnChangedAsync(object sender, FileSystemEventArgs e, CancellationToken ct)
-        {
-            await _semaphore.WaitAsync(ct);
-            try
-            {
-                throw new NotImplementedException();
-            }
-            finally
-            {
-                _semaphore.Release();
-            }
-        }
-
-        private async Task OnCreatedAsync(object sender, FileSystemEventArgs e, CancellationToken ct)
-        {
-            await _semaphore.WaitAsync(ct);
-            try
-            {
-                throw new NotImplementedException();
-            }
-            finally
-            {
-                _semaphore.Release();
-            }
-        }
-
-        private async Task OnDeletedAsync(object sender, FileSystemEventArgs e, CancellationToken ct)
-        {
-            await _semaphore.WaitAsync(ct);
-            try
-            {
-                var fileRemoved = e.FullPath;
-                var appBinaryToRemove = ;
-                if (appBinaryToRemove != null)
-                {
-                    _dbContext.AppBinaries.Remove(appBinaryToRemove);
-                    await _dbContext.SaveChangesAsync(ct);
-                }
-            }
-            finally
-            {
-                _semaphore.Release();
-            }
-        }
-
-        private async Task OnRenamedAsync(object sender, RenamedEventArgs e, CancellationToken ct)
-        {
-            await _semaphore.WaitAsync(ct);
-            try
-            {
-                throw new NotImplementedException();
-            }
-            finally
-            {
-                _semaphore.Release();
-            }
+            throw new NotImplementedException();
         }
     }
 }
---
+++ Sentinel/Workers/FSWatchWorker.cs
@@ -0,0 +1,140 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Sentinel.Workers
+{
+    public class FSWatchWorker : BackgroundService
+    {
+        private readonly SemaphoreSlim _semaphore = new(1, 1);
+
+        private readonly ILogger<FSWatchWorker> _logger;
+        private readonly SentinelDbContext _dbContext;
+        private readonly FSScanWorker _fsScanWorker;
+
+        private readonly string _libraryFolder;
+
+        public FSWatchWorker(ILogger<FSWatchWorker> logger, SentinelDbContext dbContext, FSScanWorker fSScanWorker, string libraryFolder)
+        {
+            _logger = logger;
+            _dbContext = dbContext;
+            _fsScanWorker = fSScanWorker;
+
+            _libraryFolder = libraryFolder;
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            var tcs = new TaskCompletionSource();
+
+            _fsScanWorker.ExecuteAsync(stoppingToken);
+
+            using (var watcher = new FileSystemWatcher(_libraryFolder))
+            {
+                watcher.NotifyFilter = NotifyFilters.DirectoryName
+                                     | NotifyFilters.FileName
+                                     | NotifyFilters.LastWrite
+                                     | NotifyFilters.Size;
+
+                watcher.Changed += async (sender, e) => await OnChangedAsync(sender, e, stoppingToken);
+                watcher.Created += async (sender, e) => await OnCreatedAsync(sender, e, stoppingToken);
+                watcher.Deleted += async (sender, e) => await OnDeletedAsync(sender, e, stoppingToken);
+                watcher.Renamed += async (sender, e) => await OnRenamedAsync(sender, e, stoppingToken);
+                watcher.Error += (sender, e) => _logger.LogError(e.GetException(), "Error in FileSystemWatcher");
+
+                watcher.Filter = "*";
+                watcher.IncludeSubdirectories = true;
+                watcher.EnableRaisingEvents = true;
+
+                stoppingToken.Register(() =>
+                {
+                    watcher.EnableRaisingEvents = false;
+                    tcs.SetResult();
+                });
+            }
+
+            return tcs.Task;
+        }
+
+        private async Task OnChangedAsync(object sender, FileSystemEventArgs e, CancellationToken ct)
+        {
+            try
+            {
+                await _semaphore.WaitAsync(ct);
+                throw new NotImplementedException();
+            }
+            finally
+            {
+                _semaphore.Release();
+            }
+        }
+
+        private async Task OnCreatedAsync(object sender, FileSystemEventArgs e, CancellationToken ct)
+        {
+            try
+            {
+                await _semaphore.WaitAsync(ct);
+                throw new NotImplementedException();
+            }
+            finally
+            {
+                _semaphore.Release();
+            }
+        }
+
+        private async Task OnDeletedAsync(object sender, FileSystemEventArgs e, CancellationToken ct)
+        {
+            try
+            {
+                await _semaphore.WaitAsync(ct);
+                var fileRemoved = e.FullPath;
+                var appBinary = await _dbContext.AppBinaries
+                    .Include(x => x.Files)
+                    .ThenInclude(x => x.Chunks)
+                    .SingleOrDefaultAsync(x => x.Files.Any(y => y.FilePath == fileRemoved), ct);
+                appBinary?.Files.Remove(appBinary.Files.Single(x => x.FilePath == fileRemoved));
+                if (appBinary?.Files.Count == 0)
+                {
+                    _dbContext.AppBinaries.Remove(appBinary);
+                }
+                await _dbContext.SaveChangesAsync(ct);
+            }
+            catch (OperationCanceledException ex)
+            {
+                _logger.LogWarning(ex, "OnDeletedAsync Operation canceled");
+            }
+            finally
+            {
+                _semaphore.Release();
+            }
+        }
+
+        private async Task OnRenamedAsync(object sender, RenamedEventArgs e, CancellationToken ct)
+        {
+            try
+            {
+                await _semaphore.WaitAsync(ct);
+                var fileChangedOldPath = e.OldFullPath;
+                var fileChangedNewPath = e.FullPath;
+                var appBinary = await _dbContext.AppBinaries
+                    .Include(x => x.Files)
+                    .ThenInclude(x => x.Chunks)
+                    .SingleOrDefaultAsync(x => x.Files.Any(y => y.FilePath == fileChangedOldPath), ct);
+                var file = appBinary?.Files.SingleOrDefault(x => x.FilePath == fileChangedOldPath);
+                if (file != null)
+                {
+                    file.FilePath = fileChangedNewPath;
+                    await _dbContext.SaveChangesAsync(ct);
+                }
+            }
+            catch (OperationCanceledException ex)
+            {
+                _logger.LogWarning(ex, "OnRenamedAsync Operation canceled");
+            }
+            finally
+            {
+                _semaphore.Release();
+            }
+        }
+    }
+}
