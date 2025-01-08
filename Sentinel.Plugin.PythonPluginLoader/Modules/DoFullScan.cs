d9ef5b8c8a1041dc305e795f4a9d9cea29690e06
Tabing010102 <tabing010102@outlook.com>
2025-01-09 00:40:06 +0800

    fix: fix python plugin loader

--- Sentinel.Plugin.PythonPluginLoader/Modules/DoFullScan.cs
+++ Sentinel.Plugin.PythonPluginLoader/Modules/DoFullScan.cs
@@ -11,27 +11,27 @@ namespace Sentinel.Plugin.PythonPluginLoader
     {
         public Task<ScanChangeResult> DoFullScanAsync(IQueryable<AppBinary> appBinaries, CancellationToken ct = default)
         {
-            using (Py.GIL())
+            ScanChangeResult result;
+            var pyScriptPath = (Config as Config)!.PythonScriptPath;
+            var pyClassName = (Config as Config)!.PythonClassName;
+            using (var scope = Py.CreateScope())
             {
-                var pyScriptPath = (Config as Config)!.PythonScriptPath;
-                var pyClassName = (Config as Config)!.PythonClassName;
-                using var scope = Py.CreateScope();
                 scope.Import(scope.Exec(Resource.PluginBase));
                 var code = File.ReadAllText(pyScriptPath);
                 scope.Import(scope.Exec(code));
                 _logger?.LogDebug($"Creating python class {pyClassName}.");
-                using var pyClass = scope.Get(pyClassName).Invoke((Config as Config)!.ToPython());
+                var pyClass = scope.Get(pyClassName).Invoke((Config as Config)!.ToPython());
                 _logger?.LogInformation($"Invoking do_full_scan method from python script.");
-                using PyObject pyReturn = pyClass.InvokeMethod("do_full_scan", JsonSerializer.Serialize(appBinaries, s_jso).ToPython());
+                PyObject pyReturn = pyClass.InvokeMethod("do_full_scan", JsonSerializer.Serialize(appBinaries, s_jso).ToPython());
                 _logger?.LogInformation($"Converting python return to ScanChangeResult object.");
                 if (pyReturn.AsManagedObject(typeof(string)) is not string resultJson)
                 {
                     throw new Exception("Python script must return a string object.");
                 }
-                var result = JsonSerializer.Deserialize<ScanChangeResult>(resultJson, s_jso)
+                result = JsonSerializer.Deserialize<ScanChangeResult>(resultJson, s_jso)
                     ?? throw new Exception("Python script must return a valid ScanChangeResult object.");
-                return Task.FromResult(result);
             }
+            return Task.FromResult(result);
         }
     }
 }
