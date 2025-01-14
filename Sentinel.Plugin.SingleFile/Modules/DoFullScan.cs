05cca46044ad5adcf63ec98e493b6438c7ca8d5a
Tabing010102 <tabing010102@outlook.com>
2025-01-14 23:17:35 +0800

    chore: add error handling for SingleFile

--- Sentinel.Plugin.SingleFile/Modules/DoFullScan.cs
+++ Sentinel.Plugin.SingleFile/Modules/DoFullScan.cs
@@ -10,54 +10,99 @@ namespace Sentinel.Plugin.SingleFile
     {
         public async Task<ScanChangeResult> DoFullScanAsync(IQueryable<AppBinary> appBinariesIQ, CancellationToken ct = default)
         {
-            var appBinaries = appBinariesIQ.AsEnumerable();
-
-            // appBinariesFilePaths, fsFiles, filesToXX is full paths
-            var appBinariesFilePaths = appBinaries.Select(x => Path.Combine(Config.LibraryFolder, x.Files.Single().Path));
-            var fsFiles = Directory.EnumerateFiles(Config.LibraryFolder, "*", SearchOption.AllDirectories);
-            var filesToRemove = appBinariesFilePaths.Except(fsFiles);
-            var filesToAdd = fsFiles.Except(appBinariesFilePaths);
-
-            var appBinariesToRecheck = appBinaries.ExceptBy(filesToRemove, x => Path.Combine(Config.LibraryFolder, x.Files.Single().Path));
-
-            _logger?.LogDebug($"DoFullScanAsync: Files to remove: {string.Join(", ", filesToRemove)}");
-            var appBinariesToRemove = appBinaries.Where(x => filesToRemove.Select(x => Path.GetRelativePath(Config.LibraryFolder, x)).Contains(x.Files.Single().Path)).ToList();
-
-            var appBinariesToAdd = new List<AppBinary>(filesToAdd.Count());
-            foreach (var file in filesToAdd)
+            try
             {
-                _logger?.LogInformation($"DoFullScanAsync: Adding {file}");
-                var fileEntry = await FileEntryHelper.GetFileEntryAsync(_logger, file, Config.LibraryFolder, Config.ChunkSizeBytes, ct: ct);
-                ct.ThrowIfCancellationRequested();
-                appBinariesToAdd.Add(new AppBinary(Path.GetFileName(file), Path.GetRelativePath(file, file), fileEntry.SizeBytes, [fileEntry], Guid.NewGuid()));
-            }
+                var appBinaries = appBinariesIQ.AsEnumerable();
 
-            var appBinariesToUpdate = new List<AppBinary>();
-            foreach (var appBinary in appBinariesToRecheck)
-            {
-                var fullPath = Path.Combine(Config.LibraryFolder, appBinary.Files.Single().Path);
-                if (Config.ForceCalcDigest || File.GetLastWriteTimeUtc(fullPath) != appBinary.Files.First().LastWriteUtc)
+                // appBinariesFilePaths, fsFiles, filesToXX is full paths
+                var appBinariesFilePaths = appBinaries.Select(x => Path.Combine(Config.LibraryFolder, x.Files.Single().Path));
+                var fsFiles = Directory.EnumerateFiles(Config.LibraryFolder, "*", SearchOption.AllDirectories);
+                var filesToRemove = appBinariesFilePaths.Except(fsFiles);
+                var filesToAdd = fsFiles.Except(appBinariesFilePaths);
+                _logger?.LogDebug($"DoFullScanAsync: Files to remove: {string.Join(", ", filesToRemove)}");
+                _logger?.LogDebug($"DoFullScanAsync: Files to add: {string.Join(", ", filesToAdd)}");
+
+                var appBinariesToRecheck = appBinaries.ExceptBy(filesToRemove, x => Path.Combine(Config.LibraryFolder, x.Files.Single().Path));
+                _logger?.LogDebug($"DoFullScanAsync: Files to recheck: {string.Join(", ", appBinariesToRecheck.Select(x => x.Files.Single().Path))}");
+
+                var appBinariesToRemove = appBinaries.Where(x => filesToRemove.Select(x => Path.GetRelativePath(Config.LibraryFolder, x)).Contains(x.Files.Single().Path)).ToList();
+
+                var appBinariesToAdd = new List<AppBinary>(filesToAdd.Count());
+                foreach (var file in filesToAdd)
                 {
-                    _logger?.LogInformation("DoFullScanAsync: Rechecking " + fullPath);
-                    var fileEntry = await FileEntryHelper.GetFileEntryAsync(_logger, fullPath, Config.LibraryFolder, Config.ChunkSizeBytes, ct: ct);
-                    ct.ThrowIfCancellationRequested();
-                    if (!fileEntry.Sha256.SequenceEqual(appBinary.Files.First().Sha256))
+                    try
                     {
-                        appBinariesToUpdate.Add(new AppBinary(appBinary.Name, appBinary.Path, fileEntry.SizeBytes, [fileEntry], Guid.NewGuid()));
+                        _logger?.LogInformation($"DoFullScanAsync: Adding {file}");
+                        var fileEntry = await FileEntryHelper.GetFileEntryAsync(_logger, file, Config.LibraryFolder, Config.ChunkSizeBytes, ct: ct);
+                        ct.ThrowIfCancellationRequested();
+                        appBinariesToAdd.Add(new AppBinary(Path.GetFileName(file), Path.GetRelativePath(file, file), fileEntry.SizeBytes, [fileEntry], Guid.NewGuid()));
+                    }
+                    catch (OperationCanceledException)
+                    {
+                        throw;
+                    }
+                    catch (Exception ex)
+                    {
+                        _logger?.LogError(ex, $"Error occurred while adding {file}");
+                        continue;
                     }
                 }
-                else
-                {
-                    _logger?.LogInformation("DoFullScanAsync: Skipping " + fullPath + " because its LastWriteUtc is not changed.");
-                }
-            }
 
-            return new ScanChangeResult
+                var appBinariesToUpdate = new List<AppBinary>();
+                foreach (var appBinary in appBinariesToRecheck)
+                {
+                    try
+                    {
+                        var fullPath = Path.Combine(Config.LibraryFolder, appBinary.Files.Single().Path);
+                        if (Config.ForceCalcDigest || File.GetLastWriteTimeUtc(fullPath) != appBinary.Files.First().LastWriteUtc)
+                        {
+                            _logger?.LogDebug("DoFullScanAsync: old LastWriteUtc: " + appBinary.Files.First().LastWriteUtc.ToString("O") + 
+                                ", current LastWriteUtc: " + File.GetLastWriteTimeUtc(fullPath).ToString("O"));
+                            _logger?.LogInformation("DoFullScanAsync: Rechecking " + fullPath);
+                            var fileEntry = await FileEntryHelper.GetFileEntryAsync(_logger, fullPath, Config.LibraryFolder, Config.ChunkSizeBytes, ct: ct);
+                            ct.ThrowIfCancellationRequested();
+                            if (!fileEntry.Sha256.SequenceEqual(appBinary.Files.First().Sha256))
+                            {
+                                _logger?.LogInformation("DoFullScanAsync: Updating " + fullPath + " because its SHA256 is changed.");
+                                appBinariesToUpdate.Add(new AppBinary(appBinary.Name, appBinary.Path, fileEntry.SizeBytes, [fileEntry], Guid.NewGuid()));
+                            }
+                            else
+                            {
+                                _logger?.LogInformation("DoFullScanAsync: Not updating " + fullPath + " because its SHA256 is not changed.");
+                            }
+                        }
+                        else
+                        {
+                            _logger?.LogInformation("DoFullScanAsync: Skipping " + fullPath + " because its LastWriteUtc is not changed.");
+                        }
+                    }
+                    catch (OperationCanceledException)
+                    {
+                        throw;
+                    }
+                    catch (Exception ex)
+                    {
+                        _logger?.LogError(ex, "Error occurred while rechecking " + appBinary.Files.Single().Path);
+                        continue;
+                    }
+                }
+
+                return new ScanChangeResult
+                {
+                    AppBinariesToRemove = appBinariesToRemove,
+                    AppBinariesToAdd = appBinariesToAdd,
+                    AppBinariesToUpdate = appBinariesToUpdate
+                };
+            }
+            catch (OperationCanceledException)
             {
-                AppBinariesToRemove = appBinariesToRemove,
-                AppBinariesToAdd = appBinariesToAdd,
-                AppBinariesToUpdate = appBinariesToUpdate
-            };
+                throw;
+            }
+            catch (Exception ex)
+            {
+                _logger?.LogError(ex, "Error occurred while scanning files.");
+                return new ScanChangeResult();
+            }
         }
     }
 }
